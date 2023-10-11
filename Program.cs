67979c26d68bbdc6fbf9af29749015b04b9d1e9b
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-11 15:13:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_input_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string Name;
+
+            Console.WriteLine("Enter your name: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You have entered:Name: " + Name + " Age: " + Age);
+
+        }
+    }
+}
